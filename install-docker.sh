1c1b13a7b5e4f04a34bca5b2f656628680e0aa3b
rangwl006 <54921294+rangwl006@users.noreply.github.com>
2021-05-12 12:39:06 +0800

    Create install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# remove existing installation of Docker
+if [ "$EUID" -ne 0 ]
+  then 
+    sudo apt remove docker docker-engine docker.io containerd runc
+    sudo apt update
+    sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+         $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+         
+    sudo apt update
+    sudo apt install docker-ce docker-ce-cli containerd.io
+    
+    sudo usermod -aG ${USER} docker
+    
+    sudo systemctl restart docker
+
+else
+  apt remove docker docker-engine docker.io containerd runc
+  apt update
+  apt install apt-transport-https ca-certificates curl gnupg lsb-release
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+         $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+fi
+
